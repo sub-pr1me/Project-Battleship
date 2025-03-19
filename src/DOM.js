d0e79db24da16a4b9af567fd30aa1b528fdf411b
sub-prime <sub-pr1me9000@gmail.com>
2025-03-20 00:57:38 +0200

    Add "boardRefresh" function

--- src/DOM.js
+++ src/DOM.js
@@ -8,7 +8,36 @@ const obj = (function () {
         btnDiv: document.getElementById('btn_container'),
         nameOne: document.getElementById('p1'),
         nameTwo: document.getElementById('p2'),
-        cells: document.querySelectorAll('.cell')
+        cells: document.querySelectorAll('.cell'),
+        
+        boardRefresh: function (player, tag) {
+            let board = player.data.board;
+            for (let h = 0; h < board.length; h++) {
+                for (let v = 0; v < board[h].length; v++) {
+                    if (board[h][v]) {
+                        if (board[h][v] === 1) {
+                            this.cells.forEach((cell) => {
+                                if (cell.id === `${tag}${h}${v}`) {
+                                    cell.classList.add('ship');
+                                };                                
+                            });
+                        } else if (board[h][v] === 2) {                            
+                            this.cells.forEach((cell) => {
+                                if (cell.id === `${tag}${h}${v}`) {
+                                    cell.classList.add('hit');
+                                };
+                            });
+                        } else if (board[h][v] === 3) {
+                            this.cells.forEach((cell) => {
+                                if (cell.id === `${tag}${h}${v}`) {
+                                    cell.classList.add('miss');
+                                };
+                            });                            
+                        };
+                    };
+                };  
+            };
+        }
     };
 })();
 
